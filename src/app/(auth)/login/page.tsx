8f2c92dc0e77c9f18c9a325e1f1004aa555cd9c5
Altaiir1 <lemi25yuerekli@gmail.com>
2026-05-09 00:59:29 +0200

    [WIP] Setup basic loginform and layout page and install shadcn for ui components

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { LoginForm } from "@/components/auth/login-form";
+
+export default async function LoginPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <div className="w-full max-w-md">
+      <h1 className="text-2xl font-semibold text-center mb-8 ">Anmelden</h1>
+      <LoginForm />
+    </div>
+  );
+}
---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export function LoginForm() {
+  return <div>Login Form kommt hier</div>;
+}
