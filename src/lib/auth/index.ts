9f2576be9ecc1c744c2ff1d3254e29945fcf607c
Altaiir1 <lemi25yuerekli@gmail.com>
2026-05-08 23:34:33 +0200

    [WIP] Setup better Auth, refactor schema.ts to use BetterAuth generated tables and include generated Auth-Schema.ts to make drizzle use it

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { db } from "@/lib/db";
+import * as schema from "@/lib/db/schema";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      user: schema.users,
+    },
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
+});
+
+export type Session = typeof auth.$Infer.Session;
+export type User = typeof auth.$Infer.Session.user;
