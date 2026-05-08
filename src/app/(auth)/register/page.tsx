80830ac4b867b1d2c2f16e939dfa7d80e0f16de8
Altaiir1 <lemi25yuerekli@gmail.com>
2026-05-09 01:46:10 +0200

    [TASK] Add simple todo to register page/form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,6 +3,8 @@ import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { RegisterForm } from "@/components/auth/register-form";
 
+// TODO: Setup confirmation link/code for registration
+
 export default async function RegisterPage() {
   const session = await auth.api.getSession({
     headers: await headers(),
